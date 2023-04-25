b232af6f778575306fe7fffa9353c0c5fbcc0fb7
michaelmirhom <micharby73@gmail.com>
2023-04-25 00:37:51 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email) {
+    const person = {
+        name: name,
+        email: email
+      }
+      const conf = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify(person)
+      }
+    return fetch('http://localhost:3000/users',conf )
+    .then(response => response.json())
+    .then(data => {
+        const newId = data.id;
+      const idNode = document.createElement("p");
+      idNode.textContent = newId;
+      document.body.appendChild(idNode);
+    })
+    .catch(error => {
+        const errorMessage = document.createElement("p");
+      errorMessage.textContent = error.message;
+      document.body.appendChild(errorMessage);
+    });
+  }
